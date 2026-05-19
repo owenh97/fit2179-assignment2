22223d659cbe13e50a9e61708e5f916b0d846ff7
owenh97 <164855363+owenh97@users.noreply.github.com>
2026-05-19 16:39:44 +0800

    Update and rename js/bar_grouped_industry.vg.json to industry_bar_chart.js

---
+++ industry_bar_chart.js
@@ -0,0 +1,55 @@
+country,personName,age
+Australia,Gina Rinehart,69
+Australia,Andrew Forrest,61
+Australia,Harry Triguboff,90
+Australia,Anthony Pratt,62
+Australia,Mike Cannon-Brookes,43
+Australia,Scott Farquhar,43
+Malaysia,Quek Leng Chan,81
+Australia,Blair Parry-Okeden,72
+Australia,Richard White,68
+Malaysia,Ananda Krishnan,85
+Australia,Kerry Stokes,82
+Malaysia,Koon Poh Keong,61
+Australia,Cliff Obrecht,37
+Australia,Melanie Perkins,35
+Australia,John Gandel,88
+Australia,Lindsay Fox,85
+Malaysia,Lee Yeow Chor,56
+Australia,Jack Cowin,80
+Australia,Fiona Geminder,58
+Malaysia,Lim Kok Thay,71
+Australia,Lang Walker,77
+Australia,Solomon Lew,78
+Australia,Heloise Pratt,60
+Australia,Bob Ell,78
+Australia,Clive Palmer,69
+Australia,Bianca Rinehart,46
+Australia,Ginia Rinehart,36
+Australia,Cameron Adams,43
+Australia,Kie Chie Wong,75
+Australia,Gerry Harvey,83
+Malaysia,Lau Cho Kun,87
+Malaysia,Syed Mokhtar AlBukhary,71
+Malaysia,Lee Yeow Seng,45
+Australia,Chris Wallin,70
+Australia,Alan Wilson,82
+Australia,Bruce Wilson,76
+Australia,John Wilson,85
+Australia,Angela Bennett,79
+Malaysia,G. Gnanalingam,78
+Australia,Bruce Mathieson,79
+Australia,Terry Snow,79
+Australia,David Teoh,67
+Australia,Nigel Austin,52
+Australia,Chris Ellison,65
+Australia,Gretel Packer,57
+Australia,Denis Shtengelov,50
+Australia,John Van Lieshout,77
+Malaysia,Jeffrey Cheah,78
+Australia,Anthony Hall,67
+Australia,Michael Heine,73
+Australia,Sam Hupert,68
+Australia,Sam Tarascio,78
+Australia,Manny Stul,73
+Malaysia,Tan Yu Yeh,52
