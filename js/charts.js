36d2c9b7d15647779302dbf6f3d243fe41330150
owenh97 <164855363+owenh97@users.noreply.github.com>
2026-05-30 17:33:52 +0800

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -365,3 +365,5 @@ vegaEmbed('#top_chart', {
     "tooltip": [{"field":"name","title":"Name"},{"field":"country","title":"Country"},{"field":"wealth","title":"Wealth (USD B)","format":".1f"},{"field":"industry","title":"Industry"}]
   }
 }, embedOpts);
+
+<script src="js/charts.js"></script>
